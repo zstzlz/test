ba983681cdb4ba7b265b61e59ad16e8b6188a0d1
zstzlz <zstzlg@163.com>
2019-03-01 22:39:58 +0800

    Signed-off-by: zstzlz <zstzlg@163.com>

---
+++ tetris/js/script.js
@@ -0,0 +1,10 @@
+var socket=io('ws://localhost:3000');
+var local =new Local(socket);
+//local.start();
+var remote =new Remote(socket);
+//remote.start(2,2);
+//remote.bindEvents();
+
+socket.on('waiting',function (str) {
+    document.getElementById('waiting').innerHTML=str;
+});
---
+++ tetris/wsServer.js
@@ -0,0 +1,67 @@
+var app=require('http').createServer();
+var io=require('socket.io')(app);
+var  PORT=3000;
+//客户端计数
+var clientCount=0;
+// 用来存储客户端socket
+var socketMap={};
+app.listen(PORT);
+var bindListener=function (socket,event) {
+    socket.on(event,function (data) {
+        if(socket.clientNum % 2 == 0 ){
+           if(socketMap[socket.clientNum-1]){
+            socketMap[socket.clientNum-1].emit(event,data);
+          }
+        }else {
+            if(socketMap[socket.clientNum+1]){
+            socketMap[socket.clientNum+1].emit(event,data);
+            }
+        }
+    });
+}
+
+io.on('connection',function(socket){
+   clientCount=clientCount+1;
+   socket.clientNum=clientCount;
+   socketMap[clientCount]=socket;
+   if(clientCount % 2 ==1){
+     console.log("一人正在等……"); 
+     socket.emit('waiting','waiting for another person');
+   }else {
+       if (socketMap[(clientCount - 1)]) {
+        console.log("二人游戏中……");  
+        socket.emit('start');
+           socketMap[(clientCount - 1)].emit('start');
+       } else {
+           socket.emit('leave');
+       }
+   }
+    bindListener(socket,'init');
+    bindListener(socket,'next');
+    bindListener(socket,'rotate');
+    bindListener(socket,'right');
+    bindListener(socket,'down');
+    bindListener(socket,'left');
+    bindListener(socket,'fall');
+    bindListener(socket,'fixed');
+    bindListener(socket,'line');
+    bindListener(socket,'time');
+    bindListener(socket,'lose');
+    bindListener(socket,'bottomLines');
+    bindListener(socket,'addTailLines');
+
+    socket.on('disconnect',function () {
+        if(socket.clientNum % 2 == 0 ){
+            if(socketMap[socket.clientNum-1]){
+            socketMap[socket.clientNum-1].emit('leave');
+            }
+        }else {
+            if(socketMap[socket.clientNum+1]){
+            socketMap[socket.clientNum+1].emit('leave');
+        }
+        }
+        delete(socketMap[socket.clientNum]);
+    });
+});
+
+console.log('websocket listening on port 3000!');
